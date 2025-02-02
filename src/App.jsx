f22395207af040b6695226a7c99566484d0873d5
nunoalves22 <nuno.alves2@hotmail.com>
2025-02-02 01:22:29 +0000

    Remove useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect } from 'react';
+import { useState } from 'react';
 import { ethers } from 'ethers';
 
 console.log("Ethers library:", ethers); // Verify ethers is imported correctly
